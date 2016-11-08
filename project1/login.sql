b41a107897b4a6ffff29171485f16f9a6858a97f
Ulysses Vega <raider_aegis@yahoo.com>
2016-11-08 10:32:38 -0800

    Add files via upload

---
+++ project1/login.sql
@@ -0,0 +1,81 @@
+create table Login
+(
+patient_id varchar (8) not null,
+
+username varchar (20) not null,
+
+password varchar (20) not null, 
+
+primary key(patient_id, username)
+
+);
+
+insert into Login values('10153377', 'Boben3377', 'vdA4YEhU'); 
+insert into Login values('10171392', 'Dorell1392', 'uA9ZRCE2');
+insert into Login values('10228291', 'Pacilli8291', 'r2xpHWjy');
+insert into Login values('10273325', 'Odear3325', 'HF2buZAF');
+insert into Login values('10280649', 'Haefke0649', 'KpCNpS8R');
+insert into Login values('10371601', 'Diffey1601', 'pHtJTp9K');
+insert into Login values('10435347', 'Schnorr5347', 'KvDh8VVm');
+insert into Login values('10449699', 'Kenney9699', 'JpEE54YB');
+insert into Login values('10457847', 'Corralez7847', '7bz5raAq');
+insert into Login values('10475613', 'Bano5613', '2MmSAW2s');
+
+insert into Login values('10595054', 'Rosetti5054', 'tUXJ8Fwv');
+insert into Login values('10641095', 'Walkoviak1095', 'HnVs6QBk');
+insert into Login values('10647527', 'Hoines7527', 'z2GACFfV');
+insert into Login values('10650501', 'Nassi0501', 'DwfhTY7L');
+insert into Login values('10664914', 'Martindell4914', '2sPK4fwe');
+insert into Login values('10688735', 'Sawick8735', 'E3zLrpfu');
+insert into Login values('10699586', 'Methot9586', '85vEqX7V');
+insert into Login values('10708363', 'Porras8363', '6mHYE9jr');
+insert into Login values('10775894', 'Vongsouvanh5894', 'HP8b7wn7');
+insert into Login values('10813414', 'Belloff3414', 'a2QDm7Pr');
+
+insert into Login values('10842631', 'Visecas2631', 'h8kK525h');
+insert into Login values('10870645', 'Lusiak0645', 'xe5yvYmf');
+insert into Login values('10873767', 'Burggraaf3767', 'Pw7sJGuh');
+insert into Login values('10902811', 'Heniser2811', 'M4LYPqj4');
+insert into Login values('10933864', 'Blakes3864', 'bs7T2FfT');
+insert into Login values('11015889', 'Cascante5889', 'W92EHVh8');
+insert into Login values('11086173', 'Borson6173', 'V9YbUyTg');
+insert into Login values('11124560', 'Zoto4560', 'SSFC5cJx');
+insert into Login values('11197177', 'Gura7177', 'Yqr8CQLd');
+insert into Login values('11283103', 'Chantharath3103', 'zYvjKf4z');
+
+insert into Login values('11363532', 'Garcell3532', '3EhA2btg');
+insert into Login values('11368593', 'Bethard8593', 'hwL79euz');
+insert into Login values('11455987', 'Widmar5987', 'x9Ra6zHy');
+insert into Login values('11486245', 'Carwin6245', 'v9gPNEuf');
+insert into Login values('11520068', 'Shimko0068', 'VstKDk4k');
+insert into Login values('11697978', 'Nolfi7978', 'cC5Feed3');
+insert into Login values('11719795', 'Rybakov9795', 'rRZzep2u');
+insert into Login values('11724409', 'Puppke4409', '5gpcYH6A');
+insert into Login values('11742558', 'Boether2558', 'JNNb69pD');
+insert into Login values('11789783', 'Johnsonlee9783', 'a9eqTUj2');
+
+insert into Login values('11854417', 'Fusselman4417', 'UDE6vvYM');
+insert into Login values('11868417', 'Berete8417', 'Z5v8vma6');
+insert into Login values('12039535', 'Gummersall9535', 'T778kwzR');
+insert into Login values('12055813', 'Elsman5813', 'dydta5J8');
+insert into Login values('12130416', 'Streets0416', 'E63pjHWa');
+insert into Login values('12162148', 'Perian2148', 'RJB97aJy');
+insert into Login values('12185524', 'Tourville5524', '6z9rGaLn');
+insert into Login values('12229280', 'Kostura8280', 'S8EZcvQS');
+insert into Login values('12339563', 'Jerells9563', 'BPd6uN4z');
+insert into Login values('12368454', 'Yamanaka8454', '4ey6PYjC');
+
+insert into Login values('12375897', 'Valenti5897', 'TV3RMbzz');
+insert into Login values('12404906', 'Lefevers4906', 'KpkV2Z2C');
+insert into Login values('12461160', 'Justic1160', '9jt4T4Xx');
+insert into Login values('12498240', 'Grindall8240', 'arT2FbpW');
+insert into Login values('12516868', 'Mates6868', 'Q7XJq3a8');
+insert into Login values('12544998', 'Zrust4998', '8JzxAtZ5');
+insert into Login values('12548649', 'Cowley8649', 'XR8FvM4f');
+insert into Login values('12560433', 'Ripps0433', 'bJg3gpJx');
+insert into Login values('12686919', 'Chaves6919', 'wm7aYH8s');
+insert into Login values('12691083', 'Mazzamuto1083', 'S6Pn7kLj');
+
+
+
+  
